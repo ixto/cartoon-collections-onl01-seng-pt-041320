847e47ca19c670fddf6a91bc6b15a12d293a01de
Joey <joey.alzamora@gmail.com>
2020-04-25 19:34:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
                               
 def find_the_cheese(cheese_types)
-     cheeses = %w(cheddar gouda camembert)
+     cheese_types = %w(cheddar gouda camembert)
        cheese_types.find do |maybe_cheese|  
        cheese_types.include?(maybe_cheese)
    end
