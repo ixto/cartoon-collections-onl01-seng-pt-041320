304022902cce3036fefcda1e4fa3539d653d437f
Joey <joey.alzamora@gmail.com>
2020-04-25 19:55:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,23 +9,17 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map{|call| call.capitalize + "!"}
-
-  
- 
 end
 
 def long_planeteer_calls(calls_long)
   calls_long.any?{|call| call.size > 4}
-  
-  
-  
 end
 
                               
 def find_the_cheese(array)
     maybe_cheese = %w(cheddar gouda camembert)
-      array.find do |maybe_cheese|  
-     array.include?(array)
+     array.find do |maybe_cheese|  
+    array.include?(array)
   end
 end
   
